89d8f2b412dd3343eea99aafd3bbd39ff18370dc
patrick Rafael <patrickrafael05@gmail.com>
2022-10-27 10:24:08 -0300

    Adicionando Room, ao banco de dados

--- app/src/main/java/com/example/guests/ui/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/guests/ui/repository/GuestDataBase.kt
@@ -3,26 +3,47 @@ package com.example.guests.ui.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.guests.ui.constants.DataBaseConstants
 import com.example.guests.ui.constants.DataBaseConstants.GUEST.TABLE_NAME
 
-class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+//class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+@Database(entities = [GuestDataBase::class], version = 1)
+abstract class GuestDataBase() : RoomDatabase() {
 
-    override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(CREATE_TABLE_GUEST)
-    }
-
-
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
 
     companion object {
-        private const val VERSION = 1
-        private const val NAME = "Convidados.db"
 
-        private const val CREATE_TABLE_GUEST =
-            ("create table " + DataBaseConstants.GUEST.TABLE_NAME + " ("
-                    + DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement, "
-                    + DataBaseConstants.GUEST.COLUMNS.NAME + " text, "
-                    + DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
+        private lateinit var INSTANCE: GuestDataBase
+
+        fun getDataBase(context: Context): GuestDataBase {
+            if (!::INSTANCE.isInitialized) {
+                synchronized(GuestDataBase::class) {
+                    INSTANCE =
+                        Room.databaseBuilder(context, GuestDataBase::class.java, "Convidados.db")
+                            .addMigrations(MIGRATION_1_2)
+                            .allowMainThreadQueries()
+                            .build()
+                }
+
+            }
+            return INSTANCE
+        }
+
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+
+
+            }
+
+        }
     }
 }
+
+
+fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+
